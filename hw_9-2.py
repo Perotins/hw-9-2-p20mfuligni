3683570093a54055d3b8f87b782bdf8fdf0f084c
p20mfuligni <p20mfuligni@fairfieldprep.org>
2020-01-14 09:32:55 -0500

    add MATH TO THE UNIVERSE

---
+++ hw_9-2.py
@@ -0,0 +1,31 @@
+import math
+# Write a line of code that returns square root of 529.
+print(math.sqrt(529))
+#
+# Write a line of code that returns the sine of 0.
+print(math.sin(0))
+#
+# Write a line of code that returns the cosine of 2 pi.
+print(math.cos(2*math.pi))
+#
+# Write a line of code that returns 2.5 raised to the power of 5.
+print(math.pow(2.5, 5))
+#
+# Write a line of code that returns the natural log of e.
+print(math.log(math.e, math.e))
+#
+# Write a line of code that returns e raised to the power of 4.
+print(math.pow(math.e, 4))
+
+#
+# Write a line of code that returns the ceiling of -12.9.
+print(math.ceil(-12.9))
+#
+# Write a line of code that returns the floor of 7.25.\
+print(math.floor(7.25))
+#
+# Write a line of code that returns the natural log of 1000 times 2.
+print(math.log(math.e, 1000) * 2)
+#
+# Write a line of code that converts 180 degrees to radians.
+print(180 * (math.pi / 180))
